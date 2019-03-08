df51958cd3b79df921aaefe12623202c39dd7516
comcduarte <33262448+comcduarte@users.noreply.github.com>
2019-03-08 16:24:48 -0500

    Moved Navigation and Flash Messenger to MIDNET Module

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,13 @@
 <?php 
 
-return [];
+return [
+    'view_manager' => [
+        'template_map' => [
+            'navigation' => __DIR__ . '/../view/partials/navigation.phtml',
+            'flashmessenger' => __DIR__ . '/../view/partials/flashmessenger.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
