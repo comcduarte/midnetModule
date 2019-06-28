f42f4bda067435e875e88cdb79f35930a150f377
comcduarte <33262448+comcduarte@users.noreply.github.com>
2019-06-28 13:58:57 -0400

    Created Abstract Forms for reuse

--- config/module.config.php
+++ config/module.config.php
@@ -13,6 +13,7 @@ return [
         'template_map' => [
             'navigation' => __DIR__ . '/../view/partials/navigation.phtml',
             'flashmessenger' => __DIR__ . '/../view/partials/flashmessenger.phtml',
+            'config' => __DIR__ . '/../view/config/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
